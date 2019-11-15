b5b015f0c33f72bed1afbf045f544095aad5b620
unknown <mzgajewski@gmail.com>
2019-11-15 11:38:16 +0100

    my first repository

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+print("Jak masz na imię?")
+imie = input()
+print("Cześć", imie, "miło Cię poznać")
+print("Ile masz lat?")
+wiek = input()
+print("Z jakiego peronu dostaniesz się do Hogwartu?")
+peron = input()
+print("W wieku", wiek, "lat chyba już nie ma sensu jechać na peron", peron)
+
+      
+
+
